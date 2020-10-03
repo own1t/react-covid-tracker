915e2cc4237b1d405dd1942bbf8a21827b0dae41
Ryan Kim <iawal86@gmail.com>
2020-10-02 23:59:56 -0700

    refactored

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import {
 
 // Components
 import InfoBox from "./components/InfoBox";
+import Map from "./components/Map";
 
 // CSS
 import "./App.css";
@@ -52,28 +53,39 @@ function App() {
 
   return (
     <div className="app">
-      <div className="app__header">
-        <h1>COVID-19 Tracker</h1>
+      <div className="app__left">
+        <div className="app__header">
+          <h1>COVID-19 Tracker</h1>
 
-        <FormControl className="app__dropdown">
-          <Select
-            variant="outlined"
-            value={inputCountry}
-            onChange={onCountryChange}
-          >
-            <MenuItem value="worldwide">Worldwide</MenuItem>
-            {countries.map((country) => (
-              <MenuItem value={country.value}>{country.name}</MenuItem>
-            ))}
-          </Select>
-        </FormControl>
+          <FormControl className="app__dropdown">
+            <Select
+              variant="outlined"
+              value={inputCountry}
+              onChange={onCountryChange}
+            >
+              <MenuItem value="worldwide">Worldwide</MenuItem>
+              {countries.map((country) => (
+                <MenuItem value={country.value}>{country.name}</MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </div>
+
+        <div className="app__stats">
+          <InfoBox title="Coronavirus Cases" cases={3000} total={2000} />
+          <InfoBox title="Recovered" cases={3000} total={3000} />
+          <InfoBox title="Deaths" cases={3000} total={4000} />
+        </div>
+
+        <Map />
       </div>
 
-      <div className="app__stats">
-        <InfoBox title="Coronavirus Cases" cases={3000} total={2000} />
-        <InfoBox title="Recovered" cases={3000} total={3000} />
-        <InfoBox title="Deaths" cases={3000} total={4000} />
-      </div>
+      <Card className="app__right">
+        <CardContent>
+          <h3>Live Cases by Country</h3>
+          <h3>Worldwide New Cases</h3>
+        </CardContent>
+      </Card>
     </div>
   );
 }
---
+++ src/components/Map.js
@@ -0,0 +1,10 @@
+// React
+import React from "react";
+
+// CSS
+
+function Map() {
+  return <div className="map">Map</div>;
+}
+
+export default Map;
